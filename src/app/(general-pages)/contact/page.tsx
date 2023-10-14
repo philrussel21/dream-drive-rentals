981d4b6444d56c8615f697aa417711710d599057
philrussel21 <phil.antiporda21@gmail.com>
2023-10-15 01:06:43 +1000

    wip: init OurTeam and Contact page

---
+++ src/app/(general-pages)/contact/page.tsx
@@ -0,0 +1,16 @@
+import {Container, Hero, Region} from '@app/components/partials';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Hero title="Contact Us" />
+      <Region>
+        <Container>
+          <div>Contact us form and some deets here</div>
+        </Container>
+      </Region>
+    </div>
+  );
+};
+
+export default ContactPage;
---
+++ src/app/(general-pages)/our-team/page.tsx
@@ -0,0 +1,16 @@
+import {Container, Hero, Region} from '@app/components/partials';
+
+const OurTeamPage = () => {
+  return (
+    <div>
+      <Hero title="Our Team" />
+      <Region>
+        <Container>
+          <div>Team Members here</div>
+        </Container>
+      </Region>
+    </div>
+  );
+};
+
+export default OurTeamPage;
