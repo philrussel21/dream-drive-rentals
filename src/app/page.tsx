3f2698eab8d51b56ad696da71a2bc207b1fc5a54
philrussel21 <phil.antiporda21@gmail.com>
2023-10-14 18:01:01 +1000

    feat: init Hero partial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Hello world!</p>
-    </div>
-  );
-}
+import {Hero} from '@app/components/partials';
+import homeData from '@app/data/home.json';
+import {generateDatoTestImage} from '@growthops/ext-ts';
+
+const HomePage = (): JSX.Element => (
+  <div>
+    <Hero
+      subheading={homeData.hero.subheading}
+      heading={homeData.hero.heading}
+      synopsis={homeData.hero.synopsis}
+      actions={homeData.hero.actions}
+      heroImage={generateDatoTestImage(800, 'film', 1025)}
+    />
+  </div>
+);
+
+export default HomePage;
