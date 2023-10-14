622c28e0da97bb10284ccd565828f94bfa4b3995
philrussel21 <phil.antiporda21@gmail.com>
2023-10-14 20:15:03 +1000

    feat: update Header to be responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,27 @@
+'use client';
+
 import {LinkType} from '@app/library/types';
 import Link from 'next/link';
 import {Button} from '.';
+import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline';
+import {useCallback, useState} from 'react';
 
 type HeaderProperties = {
   links: LinkType[];
 };
 
-const Header = ({links}: HeaderProperties): JSX.Element => (
-  <nav className="py-6 fixed w-full !max-w-none">
-    <div className="flex justify-between container">
-      <Link href="/">
-        <div>
-          <p>LOGO HERE</p>
-        </div>
-      </Link>
-      <ul className="flex space-x-8">
+type MobileNavigationProps = {
+  links: LinkType[];
+  onClose: () => void;
+};
+
+const MobileNavigation = ({
+  links,
+  onClose,
+}: MobileNavigationProps): JSX.Element => (
+  <div className="md:hidden fixed z-50 bg-white inset-0 text-black">
+    <div className="w-full h-full bg-gray-300 relative flex justify-center items-center">
+      <ul className="flex flex-col space-y-6">
         {links.map((link) => (
           <li key={link.href}>
             <Link href={link.href}>
@@ -23,10 +30,64 @@ const Header = ({links}: HeaderProperties): JSX.Element => (
           </li>
         ))}
       </ul>
-      <Button.Semantic label="Book Now" />
+      <button
+        type="button"
+        className="absolute top-0 right-0 -translate-x-8 translate-y-8"
+        onClick={onClose}
+      >
+        <XMarkIcon className="w-12" />
+      </button>
     </div>
-  </nav>
+  </div>
 );
+
+const Header = ({links}: HeaderProperties): JSX.Element => {
+  const [isMobileNavigationOpen, setIsMobileNavigationOpen] = useState(false);
+
+  const handleMobileNavigationOpen = useCallback(() => {
+    setIsMobileNavigationOpen(true);
+  }, []);
+
+  const handleMobileNavigationClose = useCallback(() => {
+    setIsMobileNavigationOpen(false);
+  }, []);
+
+  return (
+    <nav className="py-6 fixed w-full !max-w-none z-50">
+      <div className="flex justify-between container">
+        <Link href="/">
+          <div>
+            <p>LOGO HERE</p>
+          </div>
+        </Link>
+        <div className="md:hidden">
+          <button type="button" onClick={handleMobileNavigationOpen}>
+            <span className="sr-only">Open Mobile Navigation</span>
+            <Bars3Icon className="w-6" />
+          </button>
+        </div>
+        {isMobileNavigationOpen && (
+          <MobileNavigation
+            links={links}
+            onClose={handleMobileNavigationClose}
+          />
+        )}
+        <ul className="space-x-8 hidden md:flex">
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href}>
+                <span>{link.label}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden md:block">
+          <Button.Semantic label="Book Now" />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
 
