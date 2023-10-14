47dafb6334ffb2e72d5aa9de0fe0652230fcac86
philrussel21 <phil.antiporda21@gmail.com>
2023-10-15 00:50:02 +1000

    wip: init VehicleModels page

---
+++ src/app/(general-pages)/models/page.tsx
@@ -0,0 +1,17 @@
+import {Container, Hero, Region} from '@app/components/partials';
+
+const VehicleModelsPage = () => {
+  return (
+    <div>
+      <Hero title="Vehicle Models" />
+      <Region>
+        <Container>
+          <div>Filters here</div>
+          <div>Vehicle Models here</div>
+        </Container>
+      </Region>
+    </div>
+  );
+};
+
+export default VehicleModelsPage;
