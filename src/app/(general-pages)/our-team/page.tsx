f283a108d6b1c289946b86d568fdd5ca86aa4e12
philrussel21 <phil.antiporda21@gmail.com>
2023-10-15 16:56:22 +1000

    fix: update OurTeam page

--- src/app/(general-pages)/our-team/page.tsx
+++ src/app/(general-pages)/our-team/page.tsx
@@ -1,12 +1,39 @@
 import {Container, Hero, Region} from '@app/components/partials';
+import {Image, ResponsiveImageType} from 'react-datocms';
+import ourTeamData from '@app/data/our-team.json';
+import {generateDatoTestImage} from '@growthops/ext-ts';
+
+type OurTeamCardProps = {
+  name: string;
+  position: string;
+  image: ResponsiveImageType;
+};
+
+const OurTeamCard = ({name, position, image}: OurTeamCardProps) => (
+  <div>
+    <div>
+      <Image data={image} className="mx-auto" />
+    </div>
+    <div className="text-center p-6">
+      <h4>{name}</h4>
+      <p>{position}</p>
+    </div>
+  </div>
+);
 
 const OurTeamPage = () => {
   return (
     <div>
       <Hero title="Our Team" />
       <Region>
-        <Container>
-          <div>Team Members here</div>
+        <Container className="py-20 grid sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {ourTeamData.map((member) => (
+            <OurTeamCard
+              key={member.name}
+              {...member}
+              image={generateDatoTestImage(350, 'portrait', 1025)}
+            />
+          ))}
         </Container>
       </Region>
     </div>
