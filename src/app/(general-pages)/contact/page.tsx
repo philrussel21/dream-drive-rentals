6314fec29efae1304deaf351a3d52adaffca4c41
philrussel21 <phil.antiporda21@gmail.com>
2023-11-12 19:25:24 +1000

    fix: update thank you modal bg on Contact page

--- src/app/(general-pages)/contact/page.tsx
+++ src/app/(general-pages)/contact/page.tsx
@@ -218,7 +218,7 @@ const ContactPage = (): JSX.Element => {
 								leaveFrom="opacity-100 scale-100"
 								leaveTo="opacity-0 scale-95"
 							>
-								<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-black text-brand-off-white p-6 text-left align-middle shadow-xl transition-all">
+								<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
 									<Dialog.Title>
 										<Heading label="Thank you for reaching out!" variant="heading-three" element="h3"/>
 									</Dialog.Title>
