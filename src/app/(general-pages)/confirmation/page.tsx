f8ac4be12bf904d4e0521aa0724bfdf09df4b749
philrussel21 <phil.antiporda21@gmail.com>
2023-11-13 02:04:06 +1000

    feat: init Confirmation page

---
+++ src/app/(general-pages)/confirmation/page.tsx
@@ -0,0 +1,9 @@
+import {Hero} from '@app/components/partials';
+
+const ConfirmationPage = (): JSX.Element => {
+	return (
+		<Hero title="Confirmation"/>
+	);
+};
+
+export default ConfirmationPage;
